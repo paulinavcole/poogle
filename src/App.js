492fb116a530d7c56a9afb4a2569d57c570224b0
Paulina Cole <lina.cole@gmail.com>
2022-09-11 18:37:08 -0400

    fixed routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
-import { Routes } from './components/Routes';
+import { allRoutes } from './components/Routes';
 
 const App = () => {
   const [darkTheme, setDarkTheme] = useState(false);
@@ -11,7 +11,7 @@ const App = () => {
     <div className={darkTheme ? 'dark' : ''}>
       <div className='dark:bg-gray-900 bg-gray-100 dark:text-gray-200 black min-h-screen'>
         <Navbar setDarkTheme={setDarkTheme} darkTheme={darkTheme} />
-        <Routes />
+        <allRoutes />
         <Footer />
       </div>
     </div>
--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Results } from './Results'
 
-export const Routes = () => {
-  return (
-    <div>
-        <h1>
-            Routes
-        </h1>
+export const allRoutes = () => {
+  return (                          
+    <div className='p-4 '>
+      <Routes>
+        <Route exact path='/'>
+          <Navigate to='/search' />
+        </Route>
+        <Route path={['/search', '/images', '/news', '/videos']}>
+          <Results />
+        </Route>
+      </Routes>
     </div>
   )
 }
